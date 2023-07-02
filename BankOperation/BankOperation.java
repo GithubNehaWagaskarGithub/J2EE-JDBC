57487139d349133b2724c8771f60f2aed7aef02e
GithubNehaWagaskarGithub <133528554+GithubNehaWagaskarGithub@users.noreply.github.com>
2023-07-02 22:09:19 +0530

    Add files via upload

--- BankOperation/BankOperation.java
+++ BankOperation/BankOperation.java
@@ -6,8 +6,8 @@ public class BankOperation
 {
     static Connection con= null;
     static int accountNumber = 0;
-    static String userName=null;
-    static double accountBal=0.0;
+    static String userName= null;
+    static double accountBal= 0.0;
     //-----------------------------------------------------------------------------------------------------------------------------
     static
     {
@@ -50,7 +50,7 @@ public class BankOperation
     //-------------------------------------------------------------------------------------------------------------------------------
     public static void DepositAmount(double amt) {
         accountBal=checkBalance();
-        String query="insert into bankpassbook values(?,?,?,?,?)";
+        String query="insert into bankpassbook values(?,?,?,?,?,?)";
         PreparedStatement pStmt=null;
         try {
             pStmt= con.prepareStatement(query);
@@ -62,7 +62,7 @@ public class BankOperation
             int count=pStmt.executeUpdate();
             if(count>0)
             {
-                System.out.println(amt+"Deposited Successfully");
+                System.out.println(amt+" Deposited Successfully");
             }
             else {
                 System.out.println("Something Went Wrong");
@@ -74,9 +74,10 @@ public class BankOperation
     //------------------------------------------------------------------------------------------------------------------------------
     public static void withdrawAmount(double amt)
     {
+        accountBal=checkBalance();
         if(checkBalance()>=amt)
         {
-            String query = "insert into bankpassbook values(?,?,?,?,?)";
+            String query = "insert into bankpassbook values(?,?,?,?,?,?)";
             PreparedStatement pStmt = null;
             try {
                 pStmt = con.prepareStatement(query);
@@ -87,7 +88,7 @@ public class BankOperation
                 pStmt.setInt(5, accountNumber);
                 int count = pStmt.executeUpdate();
                 if (count > 0) {
-                    System.out.println(amt + "Withdraw Successfully");
+                    System.out.println(amt + " Withdraw Successfully");
                 } else {
                     System.out.println("Something Went Wrong");
                 }
@@ -100,7 +101,8 @@ public class BankOperation
         }
     }
     //-----------------------------------------------------------------------------------------------------------------------------
-    public static double checkBalance() {
+    public static double checkBalance()
+    {
         PreparedStatement pStmt = null;
         String query = "select Total_Amount from userlogin full join bankpassbook on Account_No=Acc_No where Account_No=? order by Tran_Id desc limit 1;";
         try {
@@ -131,7 +133,81 @@ public class BankOperation
             System.out.println("Account Statement = "+userName);
             while(rs.next())
             {
-                System.out.println(rs.getInt(1)+"\t"+ rs.getDouble(2)+"\t"+ rs.getDouble(3)+"\t"+ rs.getDouble(4)+"\t"+ rs.getInt(5));
+                System.out.println(rs.getInt(1)+"\t"+ rs.getDouble(2)+"\t"+ rs.getDouble(3)+"\t"+ rs.getDouble(4)+"\t"+ rs.getInt(5)+"\t"+ rs.getInt(6));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static void transferMoney(int accNo, double amt2)
+    {
+        PreparedStatement psmt = null;
+        String query = "SELECT Total_Amount FROM bankpassbook FULL JOIN userlogin ON Acc_No=Account_No WHERE Account_No=? ORDER BY Tran_Id DESC LIMIT 1;";
+        try {
+            psmt= con.prepareStatement(query);
+            ResultSet res = null;
+            psmt.setDouble(1,accountNumber);
+            res = psmt.executeQuery();
+            if(res.next()){
+                //TOTAL BAL OF LOGGED ACCOUNT
+                double loggedAccTAmount = res.getDouble(1);
+
+                //RETRIEVE THE ACCOUNT WHICH WE WANT TO TRANSFER THE MONEY
+                int transferAccNo = accNo;
+                double transferAmt = amt2;
+                String transferQuery = "select * from bankpassbook where Acc_No=?";
+                psmt = con.prepareStatement(transferQuery);
+                psmt.setInt(1,accNo);
+                ResultSet transferRes = psmt.executeQuery();
+                if(transferRes.next()){
+                    double totalAmountOfTA = transferRes.getDouble("Total_Amount");
+                    if(loggedAccTAmount >= amt2){
+                        //reduce the transfer amt from logged-in user.
+                        double updateLoggedInBal = loggedAccTAmount - amt2;
+
+                        //add transfer amt to transfer acc
+                        double updateTransferBal = totalAmountOfTA + amt2;
+
+                        //inserting the details
+                        String insertQuery1="insert into  bankpassbook (Tran_Id,Total_Amount,Deposite,Withdraw,Acc_No,Ref_Acc_No) values(?,?,?,?,?,?)";
+                        psmt= con.prepareStatement(insertQuery1);
+                        psmt.setInt(1,0);
+                        psmt.setDouble(2,updateLoggedInBal);
+                        psmt.setDouble(3,0);
+                        psmt.setDouble(4,amt2);
+                        psmt.setInt(5,accountNumber);
+                        psmt.setDouble(6,accNo);
+                        int count = psmt.executeUpdate();
+                        if(count>0)
+                        {
+                            System.out.println(count+" Money Withdraw Successfully");
+                        }
+
+
+                        String insertQuery2="insert into  bankpassbook (Tran_Id,Total_Amount,Deposite,Withdraw,Acc_No,Ref_Acc_No) values(?,?,?,?,?,?)";
+                        psmt= con.prepareStatement(insertQuery2);
+                        psmt.setInt(1,0);
+                        psmt.setDouble(2,updateTransferBal+amt2);
+                        psmt.setDouble(3,amt2);
+                        psmt.setInt(4,0);
+                        psmt.setInt(5,accNo);
+                        psmt.setDouble(6,accountNumber);
+
+                        int count1 = psmt.executeUpdate();
+                        if(count1>0)
+                        {
+                            System.out.println(count1+" Money Deposit/Transfer Successfully");
+                        }
+
+                    }else{
+                        System.out.println("Insufficient balance");
+                    }
+                }
+
+            }else {
+                System.out.println("SOMETHING WENT WRONG");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- BankOperation/MainApp.java
+++ BankOperation/MainApp.java
@@ -22,24 +22,25 @@ public class MainApp
             System.out.println("Login Successfully!!!!");
             while(status)
             {
-                System.out.println("1.DEPOSIT AMOUNT");
-                System.out.println("2.WITHDRAW AMOUNT");
-                System.out.println("3.CHECK BALANCE");
-                System.out.println("4.ACCOUNT STATEMENT");
-                System.out.println("5.LOG OUT");
-                System.out.println("6.EXIT");
+                System.out.println("1. DEPOSIT AMOUNT");
+                System.out.println("2. WITHDRAW AMOUNT");
+                System.out.println("3. CHECK BALANCE");
+                System.out.println("4. ACCOUNT STATEMENT");
+                System.out.println("5. TRANSFER MONEY");
+                System.out.println("6. LOG OUT");
+                System.out.println("7. EXIT");
                 System.out.println("ENTER YOUR CHOICE");
                 int ch = sc.nextInt();
                 switch (ch)
                 {
                     case 1:
-                        System.out.println("Enter a Amount do you want to Deposited");
-                        double amt=sc.nextInt();
+                        System.out.println("Enter a Amount do you want to Deposited :");
+                        double amt=sc.nextDouble();
                         BankOperation.DepositAmount(amt);
                         break;
                     case 2:
-                        System.out.println("Enter a Amount do you want to Deposited");
-                        double amt1=sc.nextInt();
+                        System.out.println("Enter a Amount do you want to Withdraw :");
+                        double amt1=sc.nextDouble();
                         BankOperation.withdrawAmount(amt1);
                         break;
                     case 3:
@@ -49,23 +50,30 @@ public class MainApp
                         BankOperation.accountStatement();
                         break;
                     case 5:
-                        System.out.println("LogOut Successfully");
-                        System.out.println("Thank You for the Transaction !!");
-                        System.out.println("Have a Nice Day !!");
+                        System.out.println("Enter a Account No you want to Transfer :");
+                        int accNo=sc.nextInt();
+                        System.out.println("Enter a Amount do you want to Transfer :");
+                        double amt2=sc.nextDouble();
+                        BankOperation.transferMoney(accNo,amt2);
+                        break;
+                    case 6:
+                        System.out.println("LOGOUT SUCCESSFULLY");
+                        System.out.println("THANKS FOR THE TRANSACTION !!");
+                        System.out.println("HAVE A NICE DAY !!");
                         BankOperation.userName= null;
                         BankOperation.accountNumber=0;
                         operations();
                         break;
-                    case 6:
+                    case 7:
                         status= false;
                         break;
                     default:
-                        System.out.println("Insufficient Data");
+                        System.out.println("INSUFFICIENT DATA");
                         break;
                 }
             }
         }else{
-            System.out.println("Invalid Login");
+            System.out.println("INVALID LOGIN");
             operations();
         }
     }
